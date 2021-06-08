f0af637c27330b6425e60e2ac7aa1f8444f2c9e0
HaraDev001 <vladisl.verkeenko@gmail.com>
2021-06-08 09:14:08 +0900

    First upload

---
+++ src/components/pages/team/AddChild.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-ons-page>
+    <div>
+        <child-photo></child-photo>
+    </div>
+  </v-ons-page>
+</template>
+
+<script>
+import ChildPhoto from './component/ChildPhoto.vue';
+
+export default {
+    name: "addchild",
+    components: {
+        ChildPhoto
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
